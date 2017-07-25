2cde8c03b8e3ad42321ccf638772f50608777257
刘扬 <c1f5d1ef@outlook.com>
2017-07-26 00:17:55 +0800

    准备路由

---
+++ Messenger/Messenger/Models/HandlerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Messenger.Foundation;
+using System;
+
+namespace Messenger.Models
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HandlerAttribute : Attribute
+    {
+        private PacketGenre _genre = PacketGenre.None;
+
+        public PacketGenre Genre => _genre;
+
+        public HandlerAttribute(PacketGenre genre)
+        {
+            _genre = genre;
+        }
+    }
+}
---
+++ Messenger/Messenger/Modules/Router.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Modules
+{
+    public class Router
+    {
+        private static Router s_ins = null;
+    }
+}
